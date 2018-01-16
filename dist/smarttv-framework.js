ea3bee626854b36d4fdfe4755096f0113227043a
Serdar Ağlamış <serdaraglamis@gmail.com>
2018-01-16 19:03:48 +0300

    Updated smarttv-device-recognizer version to 0.2.0

--- dist/smarttv-framework.js
+++ dist/smarttv-framework.js
@@ -527,10 +527,12 @@ return /******/ (function(modules) { // webpackBootstrap
 	/***/ function(module, exports, __webpack_require__) {
 	
 		var map = {
-			"./web": 3,
-			"./web.js": 3,
-			"./webos": 4,
-			"./webos.js": 4
+			"./arcelik": 3,
+			"./arcelik.js": 3,
+			"./web": 4,
+			"./web.js": 4,
+			"./webos": 5,
+			"./webos.js": 5
 		};
 		function webpackContext(req) {
 			return __webpack_require__(webpackContextResolve(req));
@@ -548,6 +550,54 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 	/***/ },
 	/* 3 */
+	/***/ function(module, exports) {
+	
+		"use strict";
+		
+		Object.defineProperty(exports, "__esModule", {
+		  value: true
+		});
+		exports.default = {
+		  LEFT: 37,
+		  RIGHT: 39,
+		  UP: 38,
+		  DOWN: 40,
+		  ENTER: 13,
+		  RETURN: 461,
+		  ZERO: 48,
+		  ONE: 49,
+		  TWO: 50,
+		  THREE: 51,
+		  FOUR: 52,
+		  FIVE: 53,
+		  SIX: 54,
+		  SEVEN: 55,
+		  EIGHT: 56,
+		  NINE: 57,
+		  RED: 403,
+		  GREEN: 404,
+		  YELLOW: 405,
+		  BLUE: 406,
+		  PLAY: 415,
+		  PAUSE: 19,
+		  STOP: 413,
+		  REC: 416,
+		  FF: 417,
+		  RW: 412,
+		  TOOLS: 470,
+		  PUP: 427,
+		  PDOWN: 428,
+		  CHLIST: -1,
+		  PRECH: -1,
+		  TXTMIX: -1,
+		  FAVCH: -1,
+		  EXIT: 528,
+		  INFO: -1
+		};
+		module.exports = exports["default"];
+	
+	/***/ },
+	/* 4 */
 	/***/ function(module, exports) {
 	
 		"use strict";
@@ -589,7 +639,7 @@ return /******/ (function(modules) { // webpackBootstrap
 		module.exports = exports["default"];
 	
 	/***/ },
-	/* 4 */
+	/* 5 */
 	/***/ function(module, exports) {
 	
 		"use strict";
